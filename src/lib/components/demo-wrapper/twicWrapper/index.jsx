a564c58f5a1ba85a60acbd534125e468f1130ca1
Miguel Beignon <mbgspcii@gmail.com>
2023-09-18 12:03:41 +0200

    TwicPics becomes TwicPics by Frontify.

--- src/lib/components/demo-wrapper/twicWrapper/index.jsx
+++ src/lib/components/demo-wrapper/twicWrapper/index.jsx
@@ -35,7 +35,7 @@ export const TwicWrapper = ({ filename, children }) => {
           <h1 className="heading-primary">
             <img
               className="twicpics-logo"
-              src="https://assets.twicpics.com/demo/@twicpics-components/logos/twicpics.svg"
+              src="https://assets.twicpics.com/demo/@twicpics-components/logos/twicpics-by-frontify.svg"
               alt="TwicPics logo"
             />
             <span>x</span>
